92c699baa3a231e01cf227386ac262ac7f78e3b0
nikitagalayda <nikitagalayda@gmail.com>
2022-01-17 17:28:54 +0800

    UI refactoring

--- src/App.js
+++ src/App.js
@@ -45,13 +45,29 @@ function App() {
     <div className="App">
       {/* <Grid container direction="column" alignContent="center"> */}
       <Box width="100vw" height="100vh" marginTop="10%">
-        <Grid container direction="row">
-          <Grid width="30%" container direction="column" alignItems="center">
-            <h1>GET COMPANIES</h1>
-            <Button variant="contained" onClick={() => retrieve()}>
-              {" "}
-              GET COMPANIES{" "}
-            </Button>
+        <Grid
+          container
+          direction="row"
+          // justifyContent="center"
+          alignItems="flex-start"
+        >
+          <Grid
+            item
+            width="33%"
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <h1>GET COMPANIES</h1>
+            </Grid>
+            <Grid item>
+              <Button variant="contained" onClick={() => retrieve()}>
+                {" "}
+                GET COMPANIES{" "}
+              </Button>
+            </Grid>
             {/* <Box style={{ maxHeight: "100px", overflow: "auto" }}> */}
             {companies.length > 0 ? (
               <CompanyList companyList={companies}></CompanyList>
@@ -61,14 +77,14 @@ function App() {
             )}
             {/* </Box> */}
           </Grid>
-          <Grid item width="30%">
+          <Grid item width="33%">
             <Grid container direction="column" alignItems="center">
               <h1>ADD A COMPANY</h1>
               <NewCompanyInterface></NewCompanyInterface>
             </Grid>
           </Grid>
 
-          <Grid item width="30%">
+          <Grid item width="33%">
             <Grid container direction="column" alignItems="center">
               <h1>INTERACT</h1>
               <ContractInteractionInterface></ContractInteractionInterface>
