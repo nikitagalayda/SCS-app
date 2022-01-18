3a5df433ba59f4b9d46b0010aeaf7aefc4323164
nikita <nikitagalayda@gmail.com>
2022-01-18 11:50:12 +0800

    Updates

---
+++ precache-manifest.ece61785ad54c234bacb5c2bd963b3b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426da580bb1428d4f822fe2eea377849",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f0e317a060b2536e81c",
+    "url": "/static/css/main.54c18f54.chunk.css"
+  },
+  {
+    "revision": "635ee6ebec6a238c2375",
+    "url": "/static/js/2.bbee8b4f.chunk.js"
+  },
+  {
+    "revision": "21c5edad3f92b5f0849aacb41d1659dc",
+    "url": "/static/js/2.bbee8b4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7dfe17cd6068886d12b",
+    "url": "/static/js/3.481e9674.chunk.js"
+  },
+  {
+    "revision": "0f0e317a060b2536e81c",
+    "url": "/static/js/main.4a32f8de.chunk.js"
+  },
+  {
+    "revision": "8811ab62d274045b3cf6",
+    "url": "/static/js/runtime-main.ce9859fb.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
